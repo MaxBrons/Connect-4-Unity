9d455212db2513fdf22649cc898da7ad08e29bd9
MaxBrons <43110145+MaxBrons@users.noreply.github.com>
2024-08-12 20:49:20 +0200

    Created a tool for generating grid cell objects in the scene.

---
+++ Assets/_C4U/Scripts/Utilites/GridGenerator.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace C4U.Utilities
+{
+    public class GridGenerator : MonoBehaviour
+    {
+        public int Width => _columnCount;
+        public int Height => _rowCount;
+        public List<Transform> GridCells => _gridCells;
+
+        [SerializeField, Min(1)] private int _columnCount;
+        [SerializeField, Min(1)] private int _rowCount;
+        [SerializeField] private GameObject _gridCellPrefab;
+        [SerializeField] private Vector2 _cellMargin;
+
+        private List<Transform> _gridCells = new();
+
+        private void Awake()
+        {
+            _gridCells = transform.GetComponentsInChildren<Transform>()
+                .Where(cell => cell != transform)
+                .ToList();
+            print(_gridCells.Contains(transform));
+        }
+
+        [ContextMenu("Regenerate Grid")]
+        private void GenerateGrid()
+        {
+            Undo.RegisterFullObjectHierarchyUndo(gameObject, "Generate grid");
+
+            _gridCells = transform.GetComponentsInChildren<Transform>()
+                .Where(cell => cell != transform)
+                .ToList();
+            _gridCells.ForEach(cell => DestroyImmediate(cell.gameObject));
+
+            if (_gridCellPrefab == null)
+                return;
+
+            for (int y = 0; y < _rowCount; y++)
+            {
+                for (int x = 0; x < _columnCount; x++)
+                {
+                    float xPos = x * _cellMargin.x + x * _gridCellPrefab.transform.localScale.x;
+                    float yPos = y * _cellMargin.y + y * _gridCellPrefab.transform.localScale.y;
+
+                    var gridCell = Instantiate(_gridCellPrefab, transform);
+                    gridCell.transform.localPosition = new Vector3(xPos, yPos);
+
+
+                    _gridCells.Add(gridCell.transform);
+                }
+            }
+        }
+    }
+}
